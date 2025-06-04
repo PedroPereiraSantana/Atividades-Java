661334e364f75141eedf6768b789ad30cd4b2767
Pedro Santana <pedro.santana9905@gmail.com>
2025-06-04 16:49:53 -0300

    subindo o exercicio 16

--- src/Faccat/exercicio14ao26/Exercicio016.java
+++ src/Faccat/exercicio14ao26/Exercicio016.java
@@ -1,5 +1,21 @@
 package Faccat.exercicio14ao26;
+import java.util.Scanner;
 
 public class Exercicio016 {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o total de maçãs compradas");
+        int totalMaca = scanner.nextInt();
+        double valorFinal = 0;
+
+        if (totalMaca > 11) {
+            valorFinal = totalMaca;
+        }else {
+            valorFinal = totalMaca * 1.30;
+        }
+
+        System.out.println("O valor final ficou R$ " + valorFinal);
+    }
 }
