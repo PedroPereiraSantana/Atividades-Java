de92905f1131da34d1d4f2c0a6bb5a6149e73892
Pedro Santana <pedro.santana9905@gmail.com>
2025-06-09 15:01:00 -0300

    fazendo o exemplo.Array002

--- src/exemplos/Array002.java
+++ src/exemplos/Array002.java
@@ -1,4 +1,13 @@
 package exemplos;
 
 public class Array002 {
+    public static void main(String[] args) {
+        String[] nome = {"Pedro", "Santana", "Falko", "Pereira"};
+
+
+        for (String i: nome){
+            System.out.println(i);
+            System.out.println("/////////");
+        }
+    }
 }
